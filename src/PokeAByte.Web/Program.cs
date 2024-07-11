90279ed2d121225c7b8b5d8a22d8228fee9fc96d
Andrew Nipp <andrewnipp@gmail.com>
2024-07-11 11:30:08 -0700

    Split Program configuration into Startup.cs

--- src/PokeAByte.Web/Program.cs
+++ src/PokeAByte.Web/Program.cs
@@ -37,69 +37,11 @@ public class Program
                         fileSizeLimitBytes: 16000000,
                         outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] ({SourceContext}) {Message}{NewLine}{Exception}")
                     .Enrich.FromLogContext());
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-            builder.Services.AddMudServices();
-            
-            //Inherited services from GameHook
-            builder.Services.AddHttpClient();
-            builder.Services.AddSingleton<AppSettings>();
-            builder.Services.AddSingleton<IMapperFilesystemProvider, MapperFilesystemProvider>();
-            builder.Services.AddSingleton<IGithubApiSettings>(x =>
-            {
-                var logger = x.GetRequiredService<ILogger<GithubApiSettings>>();
-                var config = x.GetRequiredService<IConfiguration>();
-                var token = config["GITHUB_TOKEN"];
-                return GithubApiSettings.Load(logger, token);
-            });
-            builder.Services.AddSingleton<IMapperUpdateManager, MapperUpdateManager>();
-            builder.Services.AddSingleton(x =>
-            {
-                var logger = x.GetRequiredService<ILogger<MapperUpdaterSettings>>();
-                return MapperUpdaterSettings.Load(logger);
-            });
-            builder.Services.AddSingleton<IMapperArchiveManager, MapperArchiveManager>(x =>
-            {
-                var logger = x.GetRequiredService<ILogger<MapperArchiveManager>>();
-                var mapperArchiveManager = new MapperArchiveManager(logger);
-                mapperArchiveManager.GenerateArchivedList();
-                return mapperArchiveManager;
-            });
-            builder.Services.AddSingleton<IGithubRestApi, GithubRestApi>();
-            builder.Services.AddSingleton<IGameHookInstance, GameHookInstance>();
-            builder.Services.AddSingleton<ScriptConsole>();
-            builder.Services.AddSingleton<IBizhawkMemoryMapDriver, BizhawkMemoryMapDriver>();
-            builder.Services.AddSingleton<IRetroArchUdpPollingDriver, RetroArchUdpPollingDriver>();
-            builder.Services.AddSingleton<IStaticMemoryDriver, StaticMemoryDriver>();
-            builder.Services.AddSingleton<IClientNotifier, WebSocketClientNotifier>();
-            
-            builder.Services.AddSingleton<PropertyUpdateService>();
-            //PokeAByte Services
-            builder.Services.AddSingleton<MapperClientService>(x =>
-            {
-                var mapperFs = x.GetRequiredService<IMapperFilesystemProvider>();
-                var logger = x.GetRequiredService<ILogger<MapperClientService>>();
-                var clientNotif = x.GetRequiredService<IClientNotifier>();
-                var propUpdate = x.GetRequiredService<PropertyUpdateService>();
-                
-                return new MapperClientService(mapperFs, logger, CreateClient(x), clientNotif, propUpdate);
-            });
-            builder.Services.AddScoped<NavigationService>();
+            builder.Services.ConfigureServices();
             //builder.Services.AddSingleton<MapperClientService>();
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
+            app.ConfigureApp();
             
             app.Run();     
         }
@@ -113,20 +55,5 @@ public class Program
         }
     }
 
-    private static MapperClient CreateClient(IServiceProvider services)
-    {
-        /*    ILogger<MapperClient> logger,
-                IGameHookInstance instance,
-                AppSettings appSettings,
-                IBizhawkMemoryMapDriver bizhawkMemoryMapDriver,
-                IRetroArchUdpPollingDriver retroArchUdpPollingDriver,
-                IStaticMemoryDriver staticMemoryDriver*/
-        var logger = services.GetRequiredService<ILogger<MapperClient>>();
-        var instance = services.GetRequiredService<IGameHookInstance>();
-        var appSettings = services.GetRequiredService<AppSettings>();
-        var bizhawk = services.GetRequiredService<IBizhawkMemoryMapDriver>();
-        var retro = services.GetRequiredService<IRetroArchUdpPollingDriver>();
-        var staticMem = services.GetRequiredService<IStaticMemoryDriver>();
-        return new MapperClient(logger, instance, appSettings, bizhawk, retro, staticMem);
-    }
+
 }
---
+++ src/PokeAByte.Web/Startup.cs
@@ -0,0 +1,99 @@
+﻿using GameHook.Application;
+using GameHook.Domain.Interfaces;
+using GameHook.Domain.Models;
+using GameHook.Infrastructure.Drivers;
+using GameHook.Infrastructure.Drivers.Bizhawk;
+using GameHook.Infrastructure.Github;
+using GameHook.Mappers;
+using MudBlazor.Services;
+using PokeAByte.Web.ClientNotifiers;
+using PokeAByte.Web.Services;
+
+namespace PokeAByte.Web;
+
+public static class Startup
+{
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+                    // Add services to the container.
+            services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            services.AddMudServices();
+            
+            //Inherited services from GameHook
+            services.AddHttpClient();
+            services.AddSingleton<AppSettings>();
+            services.AddSingleton<IMapperFilesystemProvider, MapperFilesystemProvider>();
+            services.AddSingleton<IGithubApiSettings>(x =>
+            {
+                var logger = x.GetRequiredService<ILogger<GithubApiSettings>>();
+                var config = x.GetRequiredService<IConfiguration>();
+                var token = config["GITHUB_TOKEN"];
+                return GithubApiSettings.Load(logger, token);
+            });
+            services.AddSingleton<IMapperUpdateManager, MapperUpdateManager>();
+            services.AddSingleton(x =>
+            {
+                var logger = x.GetRequiredService<ILogger<MapperUpdaterSettings>>();
+                return MapperUpdaterSettings.Load(logger);
+            });
+            services.AddSingleton<IMapperArchiveManager, MapperArchiveManager>(x =>
+            {
+                var logger = x.GetRequiredService<ILogger<MapperArchiveManager>>();
+                var mapperArchiveManager = new MapperArchiveManager(logger);
+                mapperArchiveManager.GenerateArchivedList();
+                return mapperArchiveManager;
+            });
+            services.AddSingleton<IGithubRestApi, GithubRestApi>();
+            services.AddSingleton<IGameHookInstance, GameHookInstance>();
+            services.AddSingleton<ScriptConsole>();
+            services.AddSingleton<IBizhawkMemoryMapDriver, BizhawkMemoryMapDriver>();
+            services.AddSingleton<IRetroArchUdpPollingDriver, RetroArchUdpPollingDriver>();
+            services.AddSingleton<IStaticMemoryDriver, StaticMemoryDriver>();
+            services.AddSingleton<IClientNotifier, WebSocketClientNotifier>();
+            
+            services.AddSingleton<PropertyUpdateService>();
+            //PokeAByte Services
+            services.AddSingleton<MapperClientService>(x =>
+            {
+                var mapperFs = x.GetRequiredService<IMapperFilesystemProvider>();
+                var logger = x.GetRequiredService<ILogger<MapperClientService>>();
+                var clientNotif = x.GetRequiredService<IClientNotifier>();
+                var propUpdate = x.GetRequiredService<PropertyUpdateService>();
+                
+                return new MapperClientService(mapperFs, logger, CreateClient(x), clientNotif, propUpdate);
+            });
+            services.AddScoped<NavigationService>();
+    }
+    private static MapperClient CreateClient(IServiceProvider services)
+    {
+        /*    ILogger<MapperClient> logger,
+                IGameHookInstance instance,
+                AppSettings appSettings,
+                IBizhawkMemoryMapDriver bizhawkMemoryMapDriver,
+                IRetroArchUdpPollingDriver retroArchUdpPollingDriver,
+                IStaticMemoryDriver staticMemoryDriver*/
+        var logger = services.GetRequiredService<ILogger<MapperClient>>();
+        var instance = services.GetRequiredService<IGameHookInstance>();
+        var appSettings = services.GetRequiredService<AppSettings>();
+        var bizhawk = services.GetRequiredService<IBizhawkMemoryMapDriver>();
+        var retro = services.GetRequiredService<IRetroArchUdpPollingDriver>();
+        var staticMem = services.GetRequiredService<IStaticMemoryDriver>();
+        return new MapperClient(logger, instance, appSettings, bizhawk, retro, staticMem);
+    }
+
+    public static void ConfigureApp(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+    }
+}
