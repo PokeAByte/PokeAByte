9ef45f90ef6bb0ac7335aa84e56f840fbf11f35f
StringEpsilon <StringEpsilon@gmail.com>
2024-09-10 20:12:41 +0200

    Property implementations: Impoved "ToValue" performance minimally.
    Either by adding shortcuts or improving the implementation more thoroughly.
    
    Also: Applied filescoped namespaces.

--- src/PokeAByte.Domain/PokeAByteProperties/BooleanProperty.cs
+++ src/PokeAByte.Domain/PokeAByteProperties/BooleanProperty.cs
@@ -1,22 +1,23 @@
 ﻿using PokeAByte.Domain.Interfaces;
+using PokeAByte.Domain.PokeAByteProperties;
 
-namespace PokeAByte.Domain.PokeAByteProperties
+namespace PokeAByte.Domain;
+
+public class BooleanProperty : PokeAByteProperty, IPokeAByteProperty
 {
-    public class BooleanProperty : PokeAByteProperty, IPokeAByteProperty
+    public BooleanProperty(IPokeAByteInstance instance, PropertyAttributes variables) : base(instance, variables)
     {
-        public BooleanProperty(IPokeAByteInstance instance, PropertyAttributes variables) : base(instance, variables)
-        {
-        }
+    }
 
-        protected override byte[] FromValue(string value)
-        {
-            var booleanValue = bool.Parse(value);
-            return booleanValue == true ? new byte[] { 0x01 } : new byte[] { 0x00 };
-        }
+    protected override byte[] FromValue(string value)
+    {
+        var booleanValue = bool.Parse(value);
+        return booleanValue == true ? [0x01] : [0x00];
+    }
 
-        protected override object? ToValue(byte[] data)
-        {
-            return data[0] == 0x00 ? false : true;
-        }
+    protected override object? ToValue(byte[] data)
+    {
+        return data[0] != 0x00;
     }
 }
+
--- src/PokeAByte.Domain/PokeAByteProperties/IntegerProperty.cs
+++ src/PokeAByte.Domain/PokeAByteProperties/IntegerProperty.cs
@@ -1,32 +1,39 @@
 ﻿using PokeAByte.Domain.Interfaces;
 
-namespace PokeAByte.Domain.PokeAByteProperties
+namespace PokeAByte.Domain.PokeAByteProperties;
+
+public class IntegerProperty : PokeAByteProperty
 {
-    public class IntegerProperty : PokeAByteProperty
+    public IntegerProperty(IPokeAByteInstance instance, PropertyAttributes variables) : base(instance, variables)
     {
-        public IntegerProperty(IPokeAByteInstance instance, PropertyAttributes variables) : base(instance, variables)
-        {
-        }
+    }
 
-        protected override byte[] FromValue(string value)
-        {
-            if (Instance == null) throw new Exception("Instance is NULL.");
-            if (Instance.PlatformOptions == null) throw new Exception("Instance.PlatformOptions is NULL.");
-            if (Length == null) throw new Exception("Length is NULL.");
-            
-            var integerValue = int.Parse(value);
-            var bytes = BitConverter.GetBytes(integerValue).Take(Length ?? 0).ToArray();
-            return bytes.ReverseBytesIfLE(Instance.PlatformOptions.EndianType);
-        }
+    protected override byte[] FromValue(string value)
+    {
+        if (Instance == null) throw new Exception("Instance is NULL.");
+        if (Instance.PlatformOptions == null) throw new Exception("Instance.PlatformOptions is NULL.");
+        if (Length == null) throw new Exception("Length is NULL.");
+        var integerValue = int.Parse(value);
+        var bytes = BitConverter.GetBytes(integerValue).Take(Length ?? 0).ToArray();
+        return bytes.ReverseBytesIfLE(Instance.PlatformOptions.EndianType);
+    }
 
-        protected override object? ToValue(byte[] data)
+    protected override object? ToValue(byte[] data)
+    {
+        if (Instance == null) throw new Exception("Instance is NULL.");
+        if (Instance.PlatformOptions == null) throw new Exception("Instance.PlatformOptions is NULL.");
+        if (data.Length == 1)
         {
-            if (Instance == null) throw new Exception("Instance is NULL.");
-            if (Instance.PlatformOptions == null) throw new Exception("Instance.PlatformOptions is NULL.");
-
-            byte[] value = new byte[8];
-            Array.Copy(data.ReverseBytesIfLE(Instance.PlatformOptions.EndianType), value, data.Length);
-            return BitConverter.ToInt32(value, 0);
+            // Shortcut: With one byte, we can just cast:
+            return (int)data[0];
         }
+        if (data.Length >= 4)
+        {
+            return BitConverter.ToInt32(data.ReverseBytesIfLE(Instance.PlatformOptions.EndianType), 0);
+        }
+        // With less than 4 bytes, we have to pad the value before using the bitconverter:
+        Span<byte> value = stackalloc byte[4];
+        data.ReverseBytesIfLE(Instance.PlatformOptions.EndianType).AsSpan().CopyTo(value);
+        return BitConverter.ToInt32(value);
     }
 }
