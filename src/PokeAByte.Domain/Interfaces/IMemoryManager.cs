7c706da6cbe9434e46deb9502ba8b1dca72395b8
StringEpsilon <StringEpsilon@gmail.com>
2024-09-13 13:51:34 +0200

    IMemoryManager: Grab all the bytes in one go for the "get int" helpers.
    This saves us looking up the fragment and creating the IByteArray instance for the second to fourth byte.

--- src/PokeAByte.Domain/Interfaces/IMemoryManager.cs
+++ src/PokeAByte.Domain/Interfaces/IMemoryManager.cs
@@ -20,10 +20,34 @@
         byte get_byte(MemoryAddress memoryAddress);
         IByteArray get_bytes(MemoryAddress memoryAddress, int length);
 
-        public ushort get_uint16_le(MemoryAddress memoryAddress) => (ushort)((get_byte(memoryAddress + 0) << 0) | (get_byte(memoryAddress + 1) << 8));
-        public ushort get_uint16_be(MemoryAddress memoryAddress) => (ushort)((get_byte(memoryAddress + 0) << 8) | (get_byte(memoryAddress + 1) << 0));
-        public uint get_uint32_le(MemoryAddress memoryAddress) => (uint)((get_uint16_le(memoryAddress + 0) << 0) | (get_uint16_le(memoryAddress + 2) << 16));
-        public uint get_uint32_be(MemoryAddress memoryAddress) => (uint)((get_uint16_be(memoryAddress + 0) << 16) | (get_uint16_be(memoryAddress + 2) << 0));
+        public ushort get_uint16_le(MemoryAddress memoryAddress)
+        {
+            var bytes = get_bytes(memoryAddress, 2).Data;
+            return (ushort)((bytes[0] << 0) | (bytes[1] << 8));
+        }
+
+        public ushort get_uint16_be(MemoryAddress memoryAddress)
+        {
+            var bytes = get_bytes(memoryAddress, 2).Data;
+            return (ushort)((bytes[0] << 8) | (bytes[1] << 0));
+        }
+
+        public uint get_uint32_le(MemoryAddress memoryAddress)
+        {
+            var bytes = get_bytes(memoryAddress, 4).Data;
+            int lower = (bytes[0] << 0) | (bytes[1] << 8);
+            int upper = (bytes[2] << 0) | (bytes[3] << 8);
+            return (uint)((lower << 0) | (upper << 16));
+        }
+
+        public uint get_uint32_be(MemoryAddress memoryAddress)
+        {
+            var bytes = get_bytes(memoryAddress, 4).Data;
+            int lower = (bytes[0] << 8) | (bytes[1] << 0);
+            int upper = (bytes[2] << 8) | (bytes[3] << 0);
+            return (uint)((lower << 16) | (upper << 0));
+        }
+
         public ulong get_uint64_le(MemoryAddress memoryAddress) => (ulong)((get_uint32_le(memoryAddress + 0) << 0) | (get_uint32_le(memoryAddress + 4) << 32));
         public ulong get_uint64_be(MemoryAddress memoryAddress) => (ulong)((get_uint32_be(memoryAddress + 0) << 32) | (get_uint32_be(memoryAddress + 4) << 0));
     }
