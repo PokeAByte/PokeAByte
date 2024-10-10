7fb27f55d1585cad467467cfd1aa05ed4c98a4d5
StringEpsilon <StringEpsilon@gmail.com>
2024-10-10 20:45:24 +0200

    Documentation: IPokeAByteDriver

--- src/PokeAByte.Domain/Interfaces/IPokeAByteDriver.cs
+++ src/PokeAByte.Domain/Interfaces/IPokeAByteDriver.cs
@@ -1,5 +1,10 @@
 namespace PokeAByte.Domain.Interfaces
 {
+    /// <summary>
+    /// Emulator memory data.
+    /// </summary>
+    /// <param name="Start"> The starting address of the memory read. </param>
+    /// <param name="Data"> The bytes returned from the emulator. </param>
     public record BlockData(MemoryAddress Start, byte[] Data);
 
     /// <summary>
@@ -10,16 +15,48 @@ namespace PokeAByte.Domain.Interfaces
     /// </summary>
     public interface IPokeAByteDriver
     {
+        /// <summary>
+        /// The proper name of the emulator the driver is for.
+        /// </summary>
         string ProperName { get; }
 
+        /// <summary>
+        /// How many milliseconds the PokeAByte instance should wait in between calls to <see cref="ReadBytes"/>.
+        /// </summary>
         int DelayMsBetweenReads { get; }
 
         Task EstablishConnection();
+
+        /// <summary>
+        /// Disconnect the driver from the emulator.
+        /// </summary>
+        /// <returns> An awaitable task. </returns>
         Task Disconnect();
+
+        /// <summary>
+        /// Checks whether a connection to the emulator can established.
+        /// </summary>
+        /// <returns> <see langword="true"/> if a connection has been made. Otherwise <see langword="false"/></returns>
         Task<bool> TestConnection();
 
+        /// <summary>
+        /// Read a number of memory blocks from the emulator.
+        /// </summary>
+        /// <remarks>
+        /// Some drivers may not use the provided <paramref name="blocks"/> argument but instead specify their own
+        /// memory ranges that they read from the emulator. 
+        /// </remarks>
+        /// <param name="blocks"> Which blocks to read. </param>
+        /// <returns> An array of <see cref="BlockData"/> containing the read memory ranges. </returns>
         Task<BlockData[]> ReadBytes(IList<MemoryAddressBlock> blocks);
 
+        /// <summary>
+        /// Instruct the emulator to write bytes into the games memory.
+        /// </summary>
+        /// <param name="startingMemoryAddress"> The starting address for the write. </param>
+        /// <param name="values"> An array of bytes that the emulator should write. </param>
+        /// <param name="path"> Currently unused. </param>
+        /// <returns> An awaitable task. </returns>
         Task WriteBytes(uint startingMemoryAddress, byte[] values, string? path = null);
     }
 
--- src/PokeAByte.Infrastructure/Drivers/StaticMemoryDriver.cs
+++ src/PokeAByte.Infrastructure/Drivers/StaticMemoryDriver.cs
@@ -4,71 +4,76 @@ using PokeAByte.Domain;
 using PokeAByte.Domain.Interfaces;
 using PokeAByte.Domain.Models;
 
-namespace PokeAByte.Infrastructure.Drivers
+namespace PokeAByte.Infrastructure.Drivers;
+
+public class StaticMemoryDriver : IStaticMemoryDriver
 {
-    public class StaticMemoryDriver : IStaticMemoryDriver
+    public string ProperName => "StaticMemory";
+    public int DelayMsBetweenReads { get; } = 25;
+
+    private readonly ILogger<StaticMemoryDriver> _logger;
+    private Dictionary<uint, byte[]> MemoryFragmentLayout { get; set; } = [];
+    private bool _isConnected = false;
+    public StaticMemoryDriver(ILogger<StaticMemoryDriver> logger)
     {
-        public string ProperName => "StaticMemory";
-        public int DelayMsBetweenReads { get; } = 25;
+        _logger = logger;
+    }
 
-        private readonly ILogger<StaticMemoryDriver> _logger;
-        private Dictionary<uint, byte[]> MemoryFragmentLayout { get; set; } = [];
-        private bool _isConnected = false;
-        public StaticMemoryDriver(ILogger<StaticMemoryDriver> logger)
+    public Task EstablishConnection()
+    {
+        if (BuildEnvironment.IsDebug == false)
         {
-            _logger = logger;
+            throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
+        }
+        _isConnected = true;
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public Task Disconnect() => Task.CompletedTask;
+
+    /// <inheritdoc/>
+    public Task<bool> TestConnection()
+    {
+        return Task.FromResult(_isConnected);
+    }
+
+    /// <inheritdoc/>
+    public async Task SetMemoryFragment(string filename)
+    {
+        if (BuildEnvironment.IsDebug == false)
+        {
+            throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
         }
 
-        public Task EstablishConnection()
+        _logger.LogInformation($"Setting static memory fragment to {filename}.");
+
+        var path = Path.GetFullPath(Path.Combine(BuildEnvironment.BinaryDirectoryPokeAByteFilePath, "..", "..", "..", "..", "Data", filename));
+        if (File.Exists(path) == false) throw new Exception($"Unable to load memory container file '{filename}'.");
+
+        var contents = await File.ReadAllTextAsync(path);
+        MemoryFragmentLayout = JsonSerializer.Deserialize<Dictionary<uint, byte[]>>(contents) ?? throw new Exception("Cannot deserialize memory fragment layout.");
+    }
+
+    /// <inheritdoc/>
+    public Task<BlockData[]> ReadBytes(IList<MemoryAddressBlock> blocks)
+    {
+        if (BuildEnvironment.IsDebug == false)
         {
-            if (BuildEnvironment.IsDebug == false)
-            {
-                throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
-            }
-            _isConnected = true;
-            return Task.CompletedTask;
+            throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
         }
 
-        public Task Disconnect() => Task.CompletedTask;
-
-        public Task<bool> TestConnection()
+        return Task.FromResult(MemoryFragmentLayout.Select(x => new BlockData(x.Key, x.Value)).ToArray());
+    }
+    
+    /// <inheritdoc/>
+    public Task WriteBytes(uint startingMemoryAddress, byte[] values, string? path = null)
+    {
+        if (BuildEnvironment.IsDebug == false)
         {
-            return Task.FromResult(_isConnected);
+            throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
         }
 
-        public async Task SetMemoryFragment(string filename)
-        {
-            if (BuildEnvironment.IsDebug == false)
-            {
-                throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
-            }
-
-            _logger.LogInformation($"Setting static memory fragment to {filename}.");
-
-            var path = Path.GetFullPath(Path.Combine(BuildEnvironment.BinaryDirectoryPokeAByteFilePath, "..", "..", "..", "..", "Data", filename));
-            if (File.Exists(path) == false) throw new Exception($"Unable to load memory container file '{filename}'.");
-
-            var contents = await File.ReadAllTextAsync(path);
-            MemoryFragmentLayout = JsonSerializer.Deserialize<Dictionary<uint, byte[]>>(contents) ?? throw new Exception("Cannot deserialize memory fragment layout.");
-        }
-
-        public Task<BlockData[]> ReadBytes(IList<MemoryAddressBlock> blocks)
-        {
-            if (BuildEnvironment.IsDebug == false)
-            {
-                throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
-            }
-
-            return Task.FromResult(MemoryFragmentLayout.Select(x => new BlockData(x.Key, x.Value)).ToArray());
-        }
-        public Task WriteBytes(uint startingMemoryAddress, byte[] values, string? path = null)
-        {
-            if (BuildEnvironment.IsDebug == false)
-            {
-                throw new Exception("Static Memory Driver operations are not allowed if not in DEBUG mode.");
-            }
-
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
