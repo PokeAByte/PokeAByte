41616d27b3b0faf0aebd2fe5f4562f58e7e53801
StringEpsilon <StringEpsilon@gmail.com>
2025-08-12 06:03:49 +0200

    Fix compilation error in footer.tsx

--- src/Frontend/src/layout/Footer.tsx
+++ src/Frontend/src/layout/Footer.tsx
@@ -2,9 +2,11 @@ import { Link } from "wouter";
 
 /** Renders the footer. */
 export function Footer() {
+	// @ts-expect-error
+	const version = globalThis.__POKEABYTE_VERSION__;
 	return (
 		<footer>
-			Poke-A-Byte {globalThis.__POKEABYTE_VERSION__} 2025 ( <Link to="/license/">AGPL</Link> )
+			Poke-A-Byte {version} 2025 ( <Link to="/license/">AGPL</Link> )
 		</footer>
 	)
 }
