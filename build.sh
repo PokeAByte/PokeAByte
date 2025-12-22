f12e4e83f393ac90c88c3c54b16004a219b1f128
StringEpsilon <StringEpsilon@gmail.com>
2025-12-22 18:14:55 +0100

    General: Add a new release workflow.

--- build.sh
+++ build.sh
@@ -1,36 +1,35 @@
-#/bin/sh
+#/bin/bash
 set -e
 cd src
 
-# Build and zip PokeAByte.Web for linux:
-dotnet publish PokeAByte.Web -r linux-x64 -o artifacts/github/PokeAByte -c Release
-cd artifacts/github/
-zip Linux-x64.zip PokeAByte/**
-rm -rf PokeAByte
-cd ../..
+declare -A targets=(
+	["win-x64"]="Windows-x64.zip"
+	["linux-x64"]="Linux-x64.zip"
+	["osx-arm64"]="macOS-arm64.zip"
+	["osx-x64"]="macOS-x64.zip"
+)
 
-# And for windows:
-dotnet publish PokeAByte.Web -r win-x64 -o artifacts/github/PokeAByte -c Release
-cd artifacts/github/
-zip Windows-x64.zip PokeAByte/**
-rm -rf PokeAByte
-cd ../..
+build () {
+	dotnet publish PokeAByte.Web -r $1 -o artifacts/github/PokeAByte -c Release
+	zip artifacts/github/${targets[$1]} artifacts/github/PokeAByte/**
+	rm -rf artifacts/github/PokeAByte
+}
 
-# macOS (ARM):
-dotnet publish PokeAByte.Web -r osx-arm64 -o artifacts/github/PokeAByte -c Release
-cd artifacts/github/
-zip macOS-arm64.zip PokeAByte/**
-rm -rf PokeAByte
-cd ../..
-
-# macOS (x86):
-dotnet publish PokeAByte.Web -r osx-x64 -o artifacts/github/PokeAByte -c Release
-cd artifacts/github/
-zip macOS-x64.zip PokeAByte/**
-rm -rf PokeAByte
-cd ../..
-
+for entry in "${!targets[@]}"; do
+    build $entry
+done
 
 # Build the bizhawk DLLs:
 dotnet publish PokeAByte.Protocol.BizHawk -o artifacts/github/ -c Release
 dotnet publish PokeAByte.Integrations.BizHawk -o artifacts/github/ -c Release
+
+# If "--publish" flag is set, use the github cli to create a release:
+if [[ $1 == *--publish* ]] then
+	cd artifacts/github
+	# Grab the release notes from the CHANGELOG.md via awk:
+	# Basically it gets the first line starting with "# " until the next line starting with "# ".
+	awk '/^# /{if (version) exit; version=1} version {print}' ../../../CHANGELOG.md > release_notes.md
+	# Create the github release. Argument 2 is the release tag given in the workflow input.
+	# The release is marked as a draft so that a human maintainer can make sure the script worked properly.
+	gh release create $2 *.{zip,dll} -t "Version $2" -F release_notes.md -d
+fi
